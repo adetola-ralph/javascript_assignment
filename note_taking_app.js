814685d553dc66c9a7cd543d25f5c564edfa8b02
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-05-17 16:20:44 +0100

    add the object's properties, create, listNotes & get function

--- note_taking_app.js
+++ note_taking_app.js
@@ -1,3 +1,21 @@
 function NotesApplication(author){
+	this.author = author;
+	this.notes = [];
+
+	this.create = function(note_content){
+		this.notes.push(note_content);
+	};
+
+	this.listNotes = function(){
+		for(int i=0;i<this.notes.length;i++){
+			console.log("Note ID: " + i);
+			console.log(this.notes[i]);
+			console.log("By Author " + this.author);
+		}
+	}
+
+	this.get: function(note_id){
+		return notes[note_id];
+	}
 
 }
