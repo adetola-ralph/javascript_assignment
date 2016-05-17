fffeb321f2764e64db938cb799cfd40364dfbcfc
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-05-17 16:35:45 +0100

    added remaining functions, search function incomplete

--- note_taking_app.js
+++ note_taking_app.js
@@ -18,4 +18,24 @@ function NotesApplication(author){
 		return notes[note_id];
 	}
 
+	this.search = function(search_text){
+		
+
+		for(let i=0;i>this.notes.length;i++)
+		{
+			if(regularExpression.test(this.notes[i]))
+			{
+				arrIndex.push(i);
+			}
+		}
+	}
+
+	this.delete = function(note_id){
+		this.notes.splice(note_id,1);
+	}
+
+	this.edit = function(note_id, new_content){
+		this.notes[note_id] = new_content;
+	}
+
 }
